8fa018dc41c5b6db4ec54b244c947b5aa2743397
Muhammad <abduhalilovmuhammadjon933@gmail.com>
2024-04-23 09:43:33 +0500

    first commit

---
+++ bin/imtihon_1_oy.dart
@@ -0,0 +1,5 @@
+import 'package:imtihon_1_oy/imtihon_1_oy.dart' as imtihon_1_oy;
+
+void main(List<String> arguments) {
+  print('Hello world: ${imtihon_1_oy.calculate()}!');
+}
---
+++ test/imtihon_1_oy_test.dart
@@ -0,0 +1,8 @@
+import 'package:imtihon_1_oy/imtihon_1_oy.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
